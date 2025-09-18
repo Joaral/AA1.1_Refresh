b08dcb546d87f01f252e1b79b01886b64fe72da8
Joaral <joan.ariza@enti.cat>
2025-09-18 20:31:53 +0200

    hola

---
+++ Torreta_Game/Assets/Scripts/Controls_Torreta.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Controls_Torreta : MonoBehaviour
+{
+    public Transform pivoteCuerpo;
+    public Transform pivoteCañon;
+    public Vector2 delta;
+    public float deltaY;
+    public float deltaX;
+
+    void Start()
+    {
+       
+    }
+
+    void Update()
+    {
+        delta = Mouse.current.delta.ReadValue();
+        deltaX = delta.x;
+        deltaY = delta.y;
+
+        pivoteCuerpo.Rotate(0f, deltaX, 0f);
+
+
+        pivoteCañon.Rotate(0f, 0f, deltaY);
+            
+    }
+
+}
